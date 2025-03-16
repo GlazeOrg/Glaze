cfb3081d144d8c7ec500182017c2338634c6f371
中下游 <287276293@qq.com>
2025-03-17 01:31:57 +0800

    chore: 修改build脚本。

--- build.bat
+++ build.bat
@@ -1,7 +1,12 @@
 @echo off
+copy ".\vcpkg.json" ".\vcpkg\vcpkg.json"
+cd vcpkg
+vcpkg x-update-baseline --add-initial-baseline
+vcpkg install
+cd ..
 mkdir build
 cd build
-cmake -B build -S . -DOpenCV_DIR=./vcpkg/vcpkg_installed/x64-windows/share/opencv
+cmake -B build
 cmake --build build --config=Release
 cmake -G "Visual Studio 17 2022" ..
 cd ..
