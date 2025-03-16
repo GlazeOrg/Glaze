dcec5af1b3b10554b76f4fce2400e9df99a009ff
中下游 <287276293@qq.com>
2025-03-17 01:11:46 +0800

    chore: 添加vcpkg依赖项，并配置opencv下载的 vcpkg.json文件。

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir build
 cd build
-cmake -B build
+cmake -B build -S . -DOpenCV_DIR=./vcpkg/vcpkg_installed/x64-windows/share/opencv
 cmake --build build --config=Release
 cmake -G "Visual Studio 17 2022" ..
 cd ..
